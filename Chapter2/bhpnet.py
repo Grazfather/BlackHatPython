13c5e7383dbb59ef8ce57bcc9a619072edf61abf
Grazfather <Grazfather@gmail.com>
2015-04-29 00:25:40 -0400

    Chapter2: bhpnet.py: Add server functionality

--- Chapter2/bhpnet.py
+++ Chapter2/bhpnet.py
@@ -15,7 +15,99 @@ port = 0
 
 
 def server_loop():
-    pass
+    global target
+
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # spin off a thread to handle our new client'
+        client_thread = threading.Thread(target=client_handler,
+                                          args=(client_socket,))
+        client_thread.start()
+
+
+def run_command(command):
+
+    # trim the newline
+    command = command.rstrip()
+
+    # run the command and get the output back
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT,
+                                         shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    # send the output back to the client
+    return output
+
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    # check for upload
+    if len(upload_destination):
+
+        # read in all of the bytes and write to our destination
+        file_buffer = ""
+
+        # keep reading data until none is available
+        while True:
+            data = client_socket.recv(1024)
+
+            if not data:
+                break
+            else:
+                file_buffer += data
+
+        # now we take these bytes and try to write them out
+        try:
+            file_descriptor = open(upload_destination, "wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+
+            # acknowledge that we wrote the file out
+            client_socket.send("Successfully saved file to %s\r\n" % \
+                               upload_destination)
+        except:
+            client_socket.send("Failed to save file for %s\r\n" % \
+                               upload_destination)
+
+    # check for command execution
+    if len(execute):
+
+        # run the command
+        output = run_command(execute)
+
+        client_socket.send(output)
+
+    # now we go into another loop if a command shell was requested
+    if command:
+        while True:
+            # show a simple prompt
+            client_socket.send("<BHP:#> ")
+
+            # now we receive until we see a linefeed (enter key)
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+
+            # run the command
+            response = run_command(cmd_buffer)
+
+            # send back the response
+            client_socket.send(response)
 
 
 def client_sender(buffer):
@@ -115,7 +207,7 @@ def main():
         else:
             assert False, "Unhandled Option"
 
-    # are we go ing to listen to just send data from stdin?
+    # are we going to listen or just send data from stdin?
     if not listen and len(target) and port > 0:
         # read in the buffer from the commandline
         # this will block, so send CTRL-D if not sending input
