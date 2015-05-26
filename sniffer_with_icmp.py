2aa3effe516b68870be00720fbc10531d62c9a10
Grazfather <Grazfather@gmail.com>
2015-05-25 21:41:06 -0400

    Chapter3: Add snipper with ICMP

---
+++ sniffer_with_icmp.py
@@ -0,0 +1,103 @@
+import socket
+import os
+import struct
+from ctypes import *
+
+# host to listen on
+host = "0.0.0.0"
+
+# our IP header
+class IP(Structure):
+    _fields_ = [
+        ("ihl", c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_ushort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_uint),
+        ("dst", c_uint)
+    ]
+
+    def __new__(cls, socket_buffer=None):
+        return cls.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        # human readable IP addresses
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
+class ICMP(Structure):
+    _fields_ = [
+        ("type", c_ubyte),
+        ("code", c_ubyte),
+        ("checksum", c_ushort),
+        ("unused", c_ushort),
+        ("next_hop_mtu", c_ushort),
+    ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+
+
+# create a raw socket and bind it to the public interface
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+# we want the IP headers included in the capture
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# if we're using Windows, we need to send an IOCTL to set up promiscuous mode
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+try:
+    while True:
+        # read in a packet
+        raw_buffer = sniffer.recvfrom(65565)[0]
+
+        # create an IP header from the first 20 bytes of the buffer
+        ip_header = IP(raw_buffer[0:20])
+
+        # print out the protocol that was detected and the hosts
+        print "Protocol: %s %s -> %s" % (ip_header.protocol,
+                                         ip_header.src_address,
+                                         ip_header.dst_address)
+
+        # if it's ICMP, we want it
+        if ip_header.protocol == "ICMP":
+            # calculate where our ICMP packet starts
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            # create our ICMP structure
+            icmp_header = ICMP(buf)
+
+            print "ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code)
+
+# Handle CTRL-C
+except KeyboardInterrupt:
+    # if we're using Windows, turn off promicuous mode
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
