92ef5bc40b0f3edd736b5df29eac0a0f54c6296e
Grazfather <grazfather@gmail.com>
2015-12-16 19:29:23 -0800

    Chapter9: Add tumblr exfiltrator

---
+++ Chapter9/decryptor.py
@@ -0,0 +1,27 @@
+import zlib
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+private_key = \
+"""-----BEGIN RSA PRIVATE KEY-----
+LEFT ALIGNED PRIVATE KEY
+-----END RSA PRIVATE KEY-----
+"""
+
+rsakey = RSA.importKey(private_key)
+rsakey = PKCS1_OAEP.new(rsakey)
+
+chunk_size = 256
+offset = 0
+decrypted = ""
+encrypted = base64.b64decode(encrypted)
+
+while offset < len(encrypted):
+    decrypted += rsakey.decrypt(encrypted[offset:offset+chunk_size])
+    offset += chunk_size
+
+# now we decompress to original
+plaintext = zlib.decompress(decrypted)
+
+print plaintext
