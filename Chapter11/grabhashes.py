8a185b52bc260c0790a4b4de3fb9ec511cbcf489
Grazfather <Grazfather@gmail.com>
2015-12-27 12:45:00 -0800

    Chapter11: Add grabhashes

---
+++ Chapter11/grabhashes.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import struct
+
+memory_file = "WindowsXPSP2"
+sys.path.append(os.path.join(os.path.expanduser("~"), "bin/volatility-master"))
+
+import volatility.conf as conf
+import volatility.registry as registry
+
+registry.PluginImporter()
+config = conf.ConfObject()
+
+import volatility.commands as commands
+import volatility.addrspace as addrspace
+
+config.parse_options()
+config.PROFILE = "WinXPSP2x86"
+config.LOCATION = "file://%s" % memory_file
+
+registry.register_global_options(config, commands.Command)
+registry.register_global_options(config, addrspace.BaseAddressSpace)
+
+from volatility.plugins.registry.registryapi import RegistryApi
+from volatility.plugins.registry.lsadump import HashDump
+
+registry = RegistryApi(config)
+registry.populate_offsets()
+
+sam_offset = None
+sys_offset = None
+
+for offset in registry.all_offsets:
+    if registry.all_offsets[offset].endswith("\\SAM"):
+        sam_offset = offset
+        print "[*] SAM: 0x%08x" % offset
+
+    if registry.all_offsets[offset].endswith("\\system"):
+        sys_offset = offset
+        print "[*] System: 0x%08x" % offset
+
+    if sam_offset is not None and sys_offset is not None:
+        config.sys_offset = sys_offset
+        config.sam_offset = sam_offset
+
+        hashdump = HashDump(config)
+
+        for hash in hashdump.calculate():
+            print hash
+
+        break
+
+if sam_offset is None or sys_offset is None:
+    print "[*] Falied to find the system or SAM offsets."
+
+
